5e84a3f9dccdf959f7739a3b642e84ba361d37ab
cakejune <tyq444@gmail.com>
2022-12-14 16:16:49 -0500

    commit tree b4b9581ac650f59d9a7bbb77d8abef183403aedc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bird.create!(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
+Bird.create!(name: "Grackle", species: "Quiscalus Quiscula")
+Bird.create!(name: "Common Starling", species: "Sturnus Vulgaris")
+Bird.create!(name: "Mourning Dove", species: "Zenaida Macroura")
+
+Plant.create!(name: "Aloe Vera", image: "https://images.unsplash.com/photo-1593642532971-7c4b8b0b3a0c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWxvZSUyMHZlcmElMjBwbGFudHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 10)
+Plant.create!(name: "Snake Plant", image: "https://images.unsplash.com/photo-1593642532971-7c4b8b0b3a0c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWxvZSUyMHZlcmElMjBwbGFudHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", price: 10)
